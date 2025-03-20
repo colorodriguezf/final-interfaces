f9536f53ac9455870ead918f5d7605d787c5eb0a
Julian Rodriguez Farias <colitorodriguez97@gmail.com>
2025-03-20 19:29:28 -0300

    Agrego efecto del carousel al scrolear (no lo pedia por eso use libreria) solo efecto visual

---
+++ prototipo-funcional/js/index.js
@@ -0,0 +1,55 @@
+// No se pedia en el trabajo, solo es un agregado visual
+// no lo hice con html, css y js puro por el tiempo
+$(document).ready(function() {
+    $('.carousel').slick({
+        slidesToShow: 10,
+        slidesToScroll: 1,
+        autoplay: false,
+        responsive: [
+            {
+                breakpoint: 1500,
+                settings: {
+                    slidesToShow:8
+                }
+            },
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 6
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+    let lastScrollY = window.scrollY;
+
+    $(window).on("scroll", function () {
+        let currentScrollY = window.scrollY;
+
+        if (currentScrollY > lastScrollY) {
+            $(".carousel").slick("slickNext");
+        } else if (currentScrollY < lastScrollY) {
+            $(".carousel").slick("slickPrev");
+        }
+
+        lastScrollY = currentScrollY;
+    });
+});
