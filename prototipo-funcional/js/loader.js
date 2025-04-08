b5c2c054135edb8e4286f194ec69e602f0be7b13
Julian Rodriguez Farias <colitorodriguez97@gmail.com>
2025-04-08 10:44:38 -0300

    bug menu del home

---
+++ prototipo-funcional/js/loader.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Loader
+    setTimeout(() => {
+        let loader = document.querySelector('.loader');
+        let contenido = document.querySelector('.contenedor-todo-home');
+
+        loader.style.display = 'none';
+        contenido.style.display = 'block';
+    }, 2500);
+});
